2dfe123c32bb07e4f6287ad2fabea9a873a3116c
Juarez Martins <juniorbmx13@gmail.com>
2022-11-22 21:27:18 -0300

    feat: resoluçao desafio palindromo

--- src/main/java/dio/intermediario/Desafio1.java
+++ src/main/java/dio/intermediario/Desafio1.java
@@ -1,6 +1,7 @@
 package dio.intermediario;
 /*
-    Desafios realizados no bootcamp
+    Desafios realizados no bootcamp. Enunciado no final do código.
+    Juarez Martins - 943617199
  */
 
 import java.util.Scanner;
@@ -10,11 +11,35 @@ public class Desafio1 {
 
         Scanner sc = new Scanner(System.in);
         String palavra = sc.nextLine();
-        int i, j;
+        String resposta = "FALSE";
 
-        for(i = 0; i < palavra.length(); i++){
-            
+        StringBuilder input = new StringBuilder();
+        input.append(palavra);
+        input.reverse();
+        String resultado = input.toString();
+
+        if(palavra.equals(resultado)){
+            resposta = "TRUE";
         }
+        
+        System.out.println(resposta);
 
     }
 }
+/*
+Desafio
+
+Neste desafio você deverá construir uma função que recebe uma String e identifique se a mesma é um palíndromo, ou seja, se a String é igual a ela mesma invertida. 
+
+Dado que temos a String "digital" uma vez invertida temos "latigid" que são diferentes. 
+
+Logo, NÃO é um Palíndromo. 
+
+Já se recebemos a string "radar" uma vez invertida temos "radar" que são iguais.
+Entrada
+
+A entrada consiste em um palavra.
+Saída
+
+Para cada String informada, terá uma saída de valor Booleano: TRUE, caso a palavra seja um palíndromo, ou FALSE caso a palavra NÃO seja um palíndromo.
+ */
