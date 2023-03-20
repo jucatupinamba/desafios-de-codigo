6830d974146eb9b5effa6ad3b630e8338f7b6e83
Juarez Martins <juniorbmx13@gmail.com>
2023-03-20 12:09:59 -0300

    Desafio Fábrica de Carros - Intermediário  01

--- src/main/java/dio/bancoPanJavaDeveloper/intermediario/Desafio1FabricaDeCarros.java
+++ src/main/java/dio/bancoPanJavaDeveloper/intermediario/Desafio1FabricaDeCarros.java
@@ -1,5 +1,12 @@
 package dio.bancoPanJavaDeveloper.intermediario;
 
+/*
+    O custo de um carro novo ao consumidor é a soma do custo de fábrica com a porcentagem do distribuidor e dos impostos
+    (aplicados ao custo de fábrica). O gerente de uma loja de carros gostaria de um programa para calcular o preço de um
+    carro novo para os clientes. Você receberá o custo de fábrica e as porcentagens referentes ao distribuidor e
+    os impostos e deverá escrever programa para ler esses valores e imprimir o valor final do carro.
+ */
+
 import java.util.Scanner;
 
 public class Desafio1FabricaDeCarros {
@@ -15,12 +22,16 @@ public class Desafio1FabricaDeCarros {
 
         int Distribuidor;
         int ValorImpostos;
+        int valorFinal;
 
         // TODO: Implemente uma condição que calcule a porcentagem do distribuidor e dos impostos:
 
         Distribuidor = (custoFabrica * porcentagemDistribuidor) / 100;
+        ValorImpostos = (PercentualImpostos  * custoFabrica) / 100;
+        custoConsumidor = ValorImpostos + Distribuidor;
+        valorFinal = custoConsumidor + custoFabrica;
 
 
-        System.out.println();
+        System.out.println(valorFinal);
     }
 }
